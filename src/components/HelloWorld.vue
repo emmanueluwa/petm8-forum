f8f4de9d582b6b9ac2b39d6df0add04705ea0a42
emmanueluwa <emmanuel@fulodev.com>
2024-03-24 17:43:27 +0000

    adding ability to get threads, adding styling to threads

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,53 @@
 import sourceData from "../data.json";
 
 console.log(sourceData);
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    };
+  }
+};
 </script>
 
 <template>
-  <div><h1>Welcome to the petm8 forum</h1></div>
+  <div>
+    <div
+      v-for="thread in threads"
+      :key="thread.slug"
+      class="col-large push-top"
+    >
+      <h1>{{ thread.title }}</h1>
+
+      <div class="post-list">
+        <div v-for="postId in thread.posts" class="post" :key="postId">
+          <div class="user-info">
+            <a href="#" class="user-name">{{
+              users[posts[postId].userId].name
+            }}</a>
+
+            <a
+              ><img
+                class="avater-large"
+                :src="users[posts[postId].userId].avatar"
+                alt=""
+            /></a>
+            <p class="desktop-only text-small">107 posts</p>
+          </div>
+
+          <div class="post-content">
+            <div>{{ posts[postId].text }}</div>
+          </div>
+
+          <div class="post-date text-faded">
+            {{ posts[postId].publishedAt }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
