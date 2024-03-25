bc05c34ec90f5d89183dd3271441d1d580639997
emmanueluwa <emmanuel@fulodev.com>
2024-03-25 07:45:35 +0000

    adding thread detail page and homepage

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import ThreadReveal from "@/components/ThreadReveal";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
+      path: "/",
+      name: "Hello",
       component: HelloWorld
+    },
+    {
+      path: "/thread/:id",
+      name: "ThreadReveal",
+      component: ThreadReveal,
+      props: true // pass params
     }
-  ]
-})
+  ],
+  mode: "history"
+});
